19e96fc0054f7fe14acdfa93cac39205ef005ccb
romany waheed <romany.waheed@arpuplus.com>
2022-02-21 20:38:31 +0200

    init pet data assignment

---
+++ src/main/java/com/abdelrahman/assignment2/petDataAssignment/PetDataAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.abdelrahman.assignment2.petDataAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetDataAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetDataAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdelrahman/assignment2/petDataAssignment/PetDataAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdelrahman.assignment2.petDataAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetDataAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
